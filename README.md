145003f425250132a208a30c81b57991ec4bd22b
amitrohan <amit@kruzr.co>
2021-10-30 23:03:25 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# BitBns-investment-processor
+
+[BitBns](https://bitbns.com/trade/#/)
+Uses the [report](https://bitbns.com/trade/#/profile/trade-report) to give you data based on your actual money input
+
+# Example Coin
+ETH
+BTC
+QKC
+DOGE
+SHIB
+SAFEMOON
+XRP
+
+# Run
+1. Rename your Bit Bns Report as REPORT.CSV
+2. Run the project and give input based on inputs.
+
+```
+    npm start
+```
---
+++ index.js
@@ -0,0 +1,116 @@
+const fs = require('fs')
+var parse = require('csv-parse')
+
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const reportsPath = './reports/REPORT.csv';
+
+var getObjectsFromReports = (callback) => {
+    try {
+        fs.readFile(reportsPath, function (err, fileData) {
+            parse(fileData, {columns: true, trim: true}, callback)  
+        })
+    }catch(e){
+        console.log("Unable to find file. Please download it from https://bitbns.com/trade/#/profile/trade-report and place it as reports/report.csv");
+    }
+}
+
+var processDataList = (dataset,inrPrice) => {
+    return dataset.reduce((prev,current,curretIndex) => {
+        var _new = prev;
+        if(current.SIDE=='BUY'){
+            _new.coinBal += parseFloat(current.Crypto_Amt)
+            _new.money -= (parseFloat(current.Crypto_Amt) * parseFloat(current.Rate))
+            _new.fiat -= parseFloat(current.FIAT)
+        }else{
+            _new.coinBal -= parseFloat(current.Crypto_Amt)
+            _new.money += (parseFloat(current.Crypto_Amt) * parseFloat(current.Rate))
+            _new.fiat += parseFloat(current.FIAT)
+        }
+        
+        _new.fee += parseFloat(current.Fee)
+        _new.value = (_new.coinBal * inrPrice)
+        if(_new.value != 0)
+            _new.value -= _new.fee
+
+        return _new  
+    },{
+        fee : 0,
+        coinBal : 0,
+        money : 0,
+        fiat : 0,
+        value : 0,
+    })
+}
+
+getCoinDataFromReport = (key,report) => {
+    return report.filter( row => (row.Coin == key));
+}
+
+cryptoTradeProcessor = (coinName,inrPrice) => {
+    // Your CSV data is in an array of arrys passed to this callback as rows.
+    getObjectsFromReports(function (err, fileData) {
+        if (err) {
+            console.error(err)
+            return
+        }
+        var coinData = getCoinDataFromReport(coinName,fileData);
+        
+        var processedData = processDataList(coinData,inrPrice);
+        var output = "\n\n";
+        output += "\n======================================"; 
+        output += "\n\t" + coinName + " trade result"; 
+        output += "\n======================================"; 
+        output += "\n\nCoins Owned : " + processedData.coinBal; 
+        output += "\nCurrent Value : " + Math.abs(processedData.value);
+        output += "\n\nTotal Fees Paid : " + processedData.fee;
+        output += "\nMoney Invested (Without fee) : " + Math.abs(processedData.money);
+        output += "\nMoney Invested (With fee) : " + Math.abs(processedData.fiat);
+        output += "\n======================================"; 
+        
+        console.log(output)
+    })
+}
+var supportedCoins = [
+    "ETH",
+    "BTC",
+    "QKC",
+    "DOGE",
+    "SHIB",
+    "SAFEMOON",
+    "XRP"
+];
+
+const fetchLatestDataFromCoinGecko = (coinName) => {
+    CoinGeckoClient
+        .coins
+        .list()
+        .then(resp => {
+            if(resp.code != 200){
+                return;
+            }
+            resp.data.map( _ => {
+                if(_.symbol == coinName.toLowerCase()){
+                    CoinGeckoClient.coins.fetch(_.id, {})
+                        .then(_resp => {
+                            const inrPrice = _resp.data.market_data.current_price.inr
+                            cryptoTradeProcessor(coinName,inrPrice)
+                        });
+            }
+        })
+    })
+}
+readline.question(supportedCoins.join('\n') + `\nWhich coin?`, coinName => {
+    fetchLatestDataFromCoinGecko(coinName);
+    readline.close()
+})
+
+
+
